4b91f66deecca7ab60e4ab32ca83e31e48dae3ad
Tadeo <tadeojcocucci@gmail.com>
2024-05-12 20:54:23 -0300

    feat: BookAdmin model

--- books/admin.py
+++ books/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Book, Genre
 
-admin.site.register(Book)
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "isbn", "price", "stock")
+    list_filter = ("genres",)
+    search_fields = ("title", "author", "isbn")
+
+
+admin.site.register(Book, BookAdmin)
 admin.site.register(Genre)
